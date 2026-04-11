a4e01e3d79429ac25634f7e2bc7938e4a382ecda
anjo <57732114+anjolib@users.noreply.github.com>
2026-04-11 15:54:57 -0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "neuro 120 final project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+  };
+
+  outputs = inputs @ { self, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import inputs.nixpkgs { inherit system; };
+      python = pkgs.python313;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = [
+          (python.withPackages (ps: with ps; [
+            pip
+            jupyter
+            jupyterlab-vim
+            ipywidgets
+
+            matplotlib
+            numpy
+            scikit-learn
+            scikit-image
+            scipy
+            seaborn
+            torchvision
+            pandas
+          ]))
+        ];
+      };
+    };
+}
